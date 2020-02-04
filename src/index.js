53dbffe9dc56e63f1736ce86fe255d3d891cb775
CindyBoom <cinl.dct@gmail.com>
2020-02-03 19:52:31 -0600

    ¨todo¨

--- src/index.js
+++ src/index.js
@@ -7,10 +7,12 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
 
-const element = <h1>Hello, Platzi Badges from React!</h1>
+import './global.css';
+import App from  './components/App';
 
 const container = document.getElementById('app');
 
-ReactDOM.render(element, container);
+ReactDOM.render(<App />, container);
 
